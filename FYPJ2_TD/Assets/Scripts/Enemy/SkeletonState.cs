5fe7958dbf73c49ed761d7433ea4541e4f237628
Zhihao <>
2018-11-17 19:46:18 +0800

    Update SkeletonState

--- FYPJ2_TD/Assets/Scripts/Enemy/SkeletonState.cs
+++ FYPJ2_TD/Assets/Scripts/Enemy/SkeletonState.cs
@@ -14,6 +14,8 @@ public class SkeletonPatrol : State
 
     public override void Enter()
     {
+        thisEnemy.SetMoveSpeed(1);
+        Debug.Log("Patrol");
     }
 
     public override void Update()
@@ -23,16 +25,24 @@ public class SkeletonPatrol : State
         if (Vector3.Distance(thisEnemy.transform.position, thisEnemy.targetWaypoint.position) <= 0.1f)
         {
             thisEnemy.GetNextwaypoint();
-        }
-        else if (Vector3.Distance(thisEnemy.transform.position, GameObject.FindWithTag("Player").transform.position) < 1.0f)
-        {
-            thisEnemy.sm.SetNextState("Chase");
+            //Debug.Log("reached");
         }
         else
         {
-            thisEnemy.transform.Translate(direction.normalized * 1.0f * Time.deltaTime);
+            thisEnemy.transform.Translate(direction.normalized * thisEnemy.GetMoveSpeed() * Time.deltaTime);
+            //Debug.Log("go");
+        }
+
+        thisEnemy.targetUnit = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject unit in thisEnemy.targetUnit)
+        {
+            if (Vector3.Distance(thisEnemy.transform.position, unit.transform.position) < 1.0f)
+            {
+                thisEnemy.sm.SetNextState("Chase");
+            }
         }
     }
+
 
     public override void Exit()
     {
@@ -52,18 +62,71 @@ public class SkeletonChase : State
 
     public override void Enter()
     {
+        Debug.Log("Chase");
+        thisEnemy.SetMoveSpeed(2);
     }
 
     public override void Update()
     {
-        thisEnemy.targetEnemy = GameObject.FindWithTag("Player").transform;
-        direction = thisEnemy.targetEnemy.position - thisEnemy.transform.position;
-        thisEnemy.transform.Translate(direction.normalized * 1 * Time.deltaTime);
-
-        if (Vector3.Distance(thisEnemy.transform.position, thisEnemy.targetEnemy.position) > 1.0f)
+        thisEnemy.targetUnit = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject unit in thisEnemy.targetUnit)
         {
-            thisEnemy.targetEnemy = null;
-            thisEnemy.sm.SetNextState("Patrol");
+            if (Vector3.Distance(thisEnemy.transform.position, unit.transform.position) > 1.0f)
+            {
+                thisEnemy.sm.SetNextState("Patrol");
+            }
+            else if (Vector3.Distance(thisEnemy.transform.position, unit.transform.position) <= 0.1f)
+            {
+                thisEnemy.sm.SetNextState("Attack");
+            }
+            else
+            {
+                direction = unit.transform.position - thisEnemy.transform.position;
+                thisEnemy.transform.Translate(direction.normalized * thisEnemy.GetMoveSpeed() * Time.deltaTime);
+            }
+        }
+    }
+
+    public override void Exit()
+    {
+    }
+}
+
+public class SkeletonAttack : State
+{
+    EnemySkeleton thisEnemy;
+    float attackCooldown = 1.0f;
+
+    public SkeletonAttack(string stateID, EnemySkeleton enemy)
+    {
+        m_stateID = stateID;
+        thisEnemy = enemy;
+    }
+
+    public override void Enter()
+    {
+        Debug.Log("Attack");
+    }
+
+    public override void Update()
+    {
+        thisEnemy.targetUnit = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject unit in thisEnemy.targetUnit)
+        {           
+            if (Vector3.Distance(thisEnemy.transform.position, unit.transform.position) <= 0.1f)
+            {
+                attackCooldown -= Time.deltaTime;
+                if (attackCooldown <= 0)
+                {
+                    unit.GetComponent<Entity>().SetHealth(unit.GetComponent<Entity>().GetHealth() - thisEnemy.GetAttackDmg());
+                    attackCooldown = 1.0f;
+                    Debug.Log("Unit getting attacked");
+                }
+            }
+            else
+            {
+                thisEnemy.sm.SetNextState("Patrol");
+            }
         }
     }
 
