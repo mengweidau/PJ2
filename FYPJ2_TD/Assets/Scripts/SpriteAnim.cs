cb404ccd3bd3743879fae28194e04da6dae37260
mengweidau <mengweidau@gmail.com>
2018-11-19 19:06:42 +0800

    commit example sprite animation

---
+++ FYPJ2_TD/Assets/Scripts/SpriteAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteAnim : MonoBehaviour {
+    TargetTemp parentscript;
+    public Animator anim;
+
+    private void Awake()
+    {
+        //i fetch the parent script (can be enemy)
+        parentscript = transform.parent.GetComponent<TargetTemp>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //when parent's health is below 3, i set the lowHealthBool from this animation controller's parameter (lowhealthbool) to true
+         if (parentscript.GetHealth() < 3)
+            anim.SetBool("LowHealthBool", true);
+    }
+}
